fe79124fccfba4e7e140a035835e9be4c95f32df
Aleksandra Garlikowska <garlikowska@gmail.com>
2019-02-18 20:11:45 +0100

    modified program.js

--- app/program.js
+++ app/program.js
@@ -19,7 +19,7 @@ function getOSinfo() {
     console.log('System:', type);
     console.log('Release:', release);
     console.log('CPU model:', cpu);
-    console.log('Uptime:', timeGenerator);
+    console.log('Uptime:', uptime);
     console.log('User name:', userInfo.username);
     console.log('Home dir:', userInfo.homedir);
 }
