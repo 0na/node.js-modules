1d6bcfee5a91f6e01cfebb59e69aec71746a65ed
Aleksandra Garlikowska <garlikowska@gmail.com>
2019-02-18 19:51:08 +0100

    change console.log time

--- app/program.js
+++ app/program.js
@@ -19,7 +19,7 @@ function getOSinfo() {
     console.log('System:', type);
     console.log('Release:', release);
     console.log('CPU model:', cpu);
-    console.log('Uptime:', uptime);
+    console.log('Uptime:', timeGenerator);
     console.log('User name:', userInfo.username);
     console.log('Home dir:', userInfo.homedir);
 }
@@ -44,3 +44,7 @@ process.stdin.on('readable', function () {
         };
     }
 });
+
+exports.print = getOSinfo;
+exports.timeGenerator = timeGenerator;
+exports.getCPUDetails = getCPUDetails;
--- modules/timeGenerator.js
+++ modules/timeGenerator.js
@@ -12,4 +12,14 @@ function timeGenerator() {
     } else {
         (hours + "h " + minutes + "min " + seconds + "sec");
     };
+    var release = os.release();
+    var cpu = os.cpus()[0].model;
+    var uptime = os.uptime();
+    var userInfo = os.userInfo();
+    console.log('System:', type);
+    console.log('Release:', release);
+    console.log('CPU model:', cpu);
+    console.log('Uptime: ', uptime);
+    console.log('User name:', userInfo.username);
+    console.log('Home dir:', userInfo.homedir);
 }
